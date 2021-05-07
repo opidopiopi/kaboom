d72d76ac5eb31df98f2710eec4eb0e0e59b65817
Marvin Wunderlich <opidopiopi@gmail.com>
2021-05-07 18:15:34 +0200

    Changed link to proper original.

--- Plugins/Shortcuts/HotKeyManager.cs
+++ Plugins/Shortcuts/HotKeyManager.cs
@@ -7,7 +7,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace Plugins
 {
     /* 
-     * Origin: https://stackoverflow.com/questions/3568513/how-to-create-keyboard-shortcut-in-windows-that-call-function-in-my-app/3569097#3569097
+     * This was copied from StackOverflow: https://stackoverflow.com/a/3654821
      */
     [ExcludeFromCodeCoverage]
     public static class HotKeyManager
